8369eb290070125b9a15a8f8e8c1472b2a935792
Renan J. Saturnino <161033810+PRO-Renan-Satunino@users.noreply.github.com>
2024-05-06 14:19:17 +0000

    atividade do dia

---
+++ atvd3.py
@@ -0,0 +1,10 @@
+import os
+os.system ("cls || clear")
+
+idade = int (input("Digite sua idade: "))
+
+if idade < 18 or idade > 65:
+    print("Você não é obrigado a votar!")
+
+else :
+    print("Você é obrigado a votar!")
